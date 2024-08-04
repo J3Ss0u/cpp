ffa9b36538ddb8be5b7b1baaf86f182eb399ddcc
J3Ss0u <salmancharai07@gmail.com>
2024-08-04 15:44:47 +0100

    ex00 still prb

--- cpp0/ex00/megaphone.cpp
+++ cpp0/ex00/megaphone.cpp
@@ -6,15 +6,31 @@
 /*   By: sacharai <sacharai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 01:23:53 by sacharai          #+#    #+#             */
-/*   Updated: 2024/07/29 10:32:51 by sacharai         ###   ########.fr       */
+/*   Updated: 2024/08/04 14:59:34 by sacharai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string.h>
+#include <cctype>
+// class Player
+// {
+//     public:
+//         int x,y;  
+//         int speed;
+//         void Move(int xa, int ya)
+//         {
+//             x += xa * speed;
+//             y += ya * speed;
+//         }
+
+// };
+
+
 
 int main(int argc, char **argv)
 {
+
     if (argc == 1)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
@@ -24,8 +40,25 @@ int main(int argc, char **argv)
         for (int i = 1; i < argc; i++)
         {
             std::string arg(argv[i]);
-            std::transform(arg.begin(), arg.end(), arg.begin(), ::toupper);
-            std::cout << arg;
+            int j = 0;
+            while(arg[j] != '\0' && ((arg[j] >= 9 && arg[j] <= 13) || arg[j] == 32))
+                j++;
+            char *c = &arg[j];
+            while (*c != '\0')
+            {
+                *c = std::toupper(*c);
+                if (*c == ' ')
+                {
+                    c++;
+                    if (*c == '\0')
+                        break;
+                    else
+                        c--;
+                }
+                std::cout << *c;
+                c++;
+            }
+            // std::cout << arg;
             if (i < argc - 1)
                 std::cout << " ";
         }
